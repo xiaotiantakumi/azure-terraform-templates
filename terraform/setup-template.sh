3261babba8785553acfcc09244090b4942bb5a54
TakumiOda <xiaotiantakumi@gmail.com>
2023-09-02 14:28:29 +0900

    setup-templateの内容修正

--- terraform/setup-template.sh
+++ terraform/setup-template.sh
@@ -3,8 +3,13 @@
 export TF_VAR_vm_admin_username="test"
 export TF_VAR_vm_admin_password="p@ssw0rd1234"
 
+# 再起的にフォーマットする
+terraform fmt -recursive
+
+# 初期化からリソース作成まで
 terraform init
 terraform plan -out plan.out
 terraform apply plan.out
+
 # リソース削除
 # terraform destroy
