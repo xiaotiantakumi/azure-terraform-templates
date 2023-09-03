ade856dbb122d6b568a2d18d736629f21b54593e
TakumiOda <xiaotiantakumi@gmail.com>
2023-09-03 19:11:19 +0900

    keyvaultの設定

--- scripts/configure-keyvault.sh
+++ scripts/configure-keyvault.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+rgName="RG-XIAO-EXAMPLE"
+vmName="vm-xiao-dev-001"
 kvname="kv-xiao"
+
+ssh-keygen -t rsa -b 4096 -C "az vm ssh key" -f ~/.ssh/vm-xiao-dev-001_key -N ""
+
+# 作成した公開鍵を作成したVMに登録する
+az vm user update \
+  --resource-group $rgName \
+  --name $vmName \
+  --username xiao_test \
+  --ssh-key-value ~/.ssh/vm-xiao-dev-001_key.pub
+
 # キーボールトが存在するかどうかを確認し、存在しない場合は作成する
 kvlist=$(az keyvault list --query "[?name=='$kvname']" | jq '. | length')
 if [ "${kvlist}" -eq 0 ]; then
@@ -11,5 +23,5 @@ else
 fi
 
 # キーボールトにシークレットを設定する
-clientSecretVar="sc-xiao-client-secret"
-az keyvault secret set --vault-name $kvname --name "$clientSecretVar" --value ""
+clientSecretVar="sc-xiao-vm-ssh-key"
+az keyvault secret set --vault-name $kvname --name "$clientSecretVar" --file "~/.ssh/vm-xiao-dev-001_key"
