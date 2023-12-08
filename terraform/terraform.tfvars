6ee723e10adf39dbb5d9e6f711e6ca55a9485d4e
TakumiOda <xiaotiantakumi@gmail.com>
2023-12-08 13:40:35 +0900

    mainブランチの内容を一番簡潔にしてREADMEに内容を記載

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -2,12 +2,3 @@ default_region = "japaneast"
 prefix         = "xiao"
 padding        = "001"
 environment    = "dev"
-
-networking_settings = {
-  dns_server                 = "1.1.1.1"
-  snet_private_address_space = "10.2.18.0/26"
-  snet_bastion_address_space = "10.2.18.64/27"
-  vnet_address_space         = "10.2.18.0/24"
-
-  tags = {}
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,23 +13,3 @@ variable "padding" {
 variable "environment" {
   type = string
 }
-
-variable "vm_admin_username" {
-  description = "The admin username for the VM"
-  type        = string
-}
-
-variable "vm_admin_password" {
-  description = "The admin password for the VM"
-  type        = string
-}
-
-variable "networking_settings" {
-  type = object({
-    dns_server                 = string
-    snet_bastion_address_space = string
-    snet_private_address_space = string
-    vnet_address_space         = string
-    tags                       = map(string)
-  })
-}
