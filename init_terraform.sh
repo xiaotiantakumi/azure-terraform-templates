7eedc994aad010ebd5b5680e4208a7021169e0ab
TakumiOda <xiaotiantakumi@gmail.com>
2024-02-10 16:27:49 +0900

    Fix ARM_ACCESS_KEY export in init_terraform.sh

--- init_terraform.sh
+++ init_terraform.sh
@@ -14,5 +14,4 @@ CONTAINER_NAME=tfstate
 # az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
-export ARM_ACCESS_KEY=$ACCOUNT_KEY
 echo ARM_ACCESS_KEY=$ACCOUNT_KEY
